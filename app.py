9c45a1e2cdb30e3c0e1b4df7e934800ddc4c39c7
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-10-09 22:20:56 -0500

    Created login page

--- app.py
+++ app.py
@@ -44,5 +44,11 @@ class User(UserMixin, db.Model):
 def index():
     return render_template('index.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+    
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
